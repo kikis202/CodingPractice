08f8a2c8b5fc229ea1f63ed1ce39e068a0029118
Kristaps Kozulis <kristaps1000@gmail.com>
2022-08-17 14:46:11 +0300

    Task2 done

---
+++ src/com/practice/Task2.java
@@ -0,0 +1,28 @@
+package com.practice;
+
+/*
+2) Ask user to input two boolean values and compare them.
+    Input:
+       Enter b1: true
+       Enter b2: false
+    Expected output:
+       Is b1 and b2 equal? - false
+ */
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        /// Input
+        System.out.println("Enter b1: ");
+        boolean b1 = scanner.nextBoolean();
+
+        System.out.println("Enter b2: ");
+        boolean b2 = scanner.nextBoolean();
+
+        /// Output
+        System.out.printf("%nIs b1 equal to b2? - %b%n",b1 == b2);
+    }
+}
