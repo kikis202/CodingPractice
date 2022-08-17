6a6911ddd0a1f7dd59e1940672311460439bf747
Kristaps Kozulis <kristaps1000@gmail.com>
2022-08-17 14:51:22 +0300

    Task3 done

---
+++ src/com/practice/Task3.java
@@ -0,0 +1,22 @@
+package com.practice;
+
+/*
+3) Ask user to input a whole number and output true, if it is even number
+                                           and false, if it is odd number
+
+    Hint: use remainder operator to divide number by two and get reminder (if reminder is 0 then it means it is even number)-
+ */
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        /// Input
+        System.out.println("Enter a whole number: ");
+        int x = scanner.nextInt();
+
+        System.out.println(x % 2 == 0);
+    }
+}
