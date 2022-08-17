4b07cef7b943bbe7a9b605d49224dd108a0e5b12
Kristaps Kozulis <kristaps1000@gmail.com>
2022-08-17 14:20:34 +0300

    task1 added user input for a and b

--- src/com/practice/Task1.java
+++ src/com/practice/Task1.java
@@ -1,7 +1,27 @@
 package com.practice;
+/*
+1) Ask user to input two numbers, compare them and output result:
+   Input:
+       Enter value a: 10
+       Enter value b: 10
+   Expected output:
+       is 10 equal to 10? - true
+       is 10 less than 10? - false
+       is 10 less or equal to 10? - true
+       is 10 greater than 10? - false
+       is 10 greater or equal to 10? - true
+ */
+
+import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.printf("Enter value a: ");
+        int a = scanner.nextInt();
+
+        System.out.printf("Enter value b: ");
+        int b = scanner.nextInt();
     }
 }
