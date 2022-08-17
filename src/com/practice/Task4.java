976428bead8bbff4de0c95e677bcfeff97ed56ed
Kristaps Kozulis <kristaps1000@gmail.com>
2022-08-17 14:54:13 +0300

    Task4 done

---
+++ src/com/practice/Task4.java
@@ -0,0 +1,19 @@
+package com.practice;
+
+/*
+4) Ask user to input a whole number and output false, if it is even number and true if it is odd number
+ */
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        /// Input
+        System.out.println("Enter a whole number: ");
+        int x = scanner.nextInt();
+
+        System.out.println(x % 2 == 1);
+    }
+}
