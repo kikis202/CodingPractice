a509ab24a6520d2c2dd9cc7cfd7145d022f195eb
Kristaps Kozulis <kristaps1000@gmail.com>
2022-08-18 10:43:49 +0300

    class BankAccount updated to match Task4 requirements

--- src/com/day3/BankAccount.java
+++ src/com/day3/BankAccount.java
@@ -11,9 +11,30 @@ package com.day3;
     * create a method "printBalance" which displays the current balance to user
     * method for transfer balance from one bank account to another
  */
+/*
+4) update your solution:
+        * Encapsulate the balance property so it is private
+        * add to "withdraw" method, so it would be impossible to withdraw
+          more money than exists in bank account. If there is an attempt to withdraw
+          more money than exists in bank account, output message:
+            "There is not enough funds."
+        * Modify "transferFrom" method so it would be impossible to transfer more
+          money from the other account than it's balance.
+                For example, if bank account A has 50 units and bank account B has 25 units
+                and we try to transfer 30 units from B to A, then it should output:
+                    "Transfer canceled. You are trying to transfer 30 units, but only 25 are available."
+        * add a limit for depositing money. If the deposit is greater than 5000, then it should not
+          do the deposit and output message:
+              "Transaction canceled. Max deposit - 5000 | Your deposit: 6000"
+        * call these methods and demonstrate how they work, for example:
+            * write a code which tries to withdraw more money than there is in bank account
+            * write a code which tries to deposit more than max limit
+            * write a code which tries to transfer more money from one account to another account than possible
+ */
 
 public class BankAccount {
     private double balance;
+    private final double depositLimit = 5000;
 
     public BankAccount() {
         balance = 0;
@@ -23,38 +44,59 @@ public class BankAccount {
         balance = b;
     }
 
-    /** allows the user to increase the balance **/
+    /**
+     * allows the user to increase the balance
+     **/
     public void deposit(double amount) {
-        if (amount > 0) balance += amount;
-        else System.out.println("Error - Negative deposit");
-    }
-
-    /** allows the user to decrease the balance **/
-    public void withdraw(double amount) {
-        if (amount > 0) {
-            if (balance>=amount) balance -= amount;
-            else System.out.println("Error - insufficient funds");
-        } else {
-            System.out.println("Error - Negative withdraw");
+        if (amount <= 0) {
+            System.out.println("Error - Negative deposit");
+            return;
         }
+        if (amount > depositLimit) {
+            System.out.printf("Transaction canceled. Max deposit - %f | Your deposit: %f%n", depositLimit, amount);
+            return;
+        }
+        balance += amount;
+        System.out.printf("Successfully deposited %f | current account balance: %f%n", amount, balance);
     }
 
-    /** displays the current balance to user **/
-    public void printBalance() {
-        System.out.printf("Current balance: %f%n", balance);
-    }
-
-    /** method for transfer balance from one bank account to another **/
-    public void transferFunds(BankAccount ba, double amount) {
+    /**
+     * allows the user to decrease the balance
+     **/
+    public void withdraw(double amount) {
         if (amount <= 0) {
             System.out.println("Error - Negative withdraw");
             return;
         }
+        if (amount > balance) {
+            System.out.println("There is not enough funds.");
+            return;
+        }
+        balance -= amount;
+        System.out.printf("Successfully withdrawn %f | current account balance: %f%n", amount, balance);
+    }
+
+    /**
+     * displays the current balance to user
+     **/
+    public void printBalance() {
+        System.out.printf("Current balance: %f%n", balance);
+    }
+
+    /**
+     * method for transfer balance from one bank account to another
+     **/
+    public void transferFunds(BankAccount ba, double amount) {
+        if (amount <= 0) {
+            System.out.println("Error - Negative amount");
+            return;
+        }
         if (balance < amount) {
-            System.out.println("Error - insufficient funds");
+            System.out.printf("Transfer canceled. You are trying to transfer %f units, but only %f are available.%n", amount, balance);
             return;
         }
         this.withdraw(amount);
         ba.deposit(amount);
+        System.out.printf("Transaction successful | current account balance: %f%n", balance);
     }
 }
