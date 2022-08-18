e3f91eb6bcc3fcc32cc9dd7aa3df01bb05fc6241
Kristaps Kozulis <kristaps1000@gmail.com>
2022-08-18 11:57:24 +0300

    Created a Vehicle class

---
+++ src/com/day3/Vehicle.java
@@ -0,0 +1,28 @@
+package com.day3;
+
+/*
+5) Vehicle
+// Create a base class of a Vehicle,
+// then create a Car class which inherits from the Vehicle class.
+// Also, create another class, a specific type of Car that inherits from the Car class.
+// Program should be able to handle steering, changing gears, and drive (having speed in other words).
+// You might decide where to put the appropriate state and behaviors (fields and methods).
+// As mentioned above, changing gears, increasing/decreasing speed of driving should be included.
+ */
+
+public class Vehicle {
+    private boolean engineIgnited = false;
+
+
+    public Vehicle() {
+
+    }
+
+    public Vehicle(boolean engineIgnited) {
+        this.engineIgnited = engineIgnited;
+    }
+
+    public void toggleEngine() {
+        engineIgnited = !engineIgnited;
+    }
+}
